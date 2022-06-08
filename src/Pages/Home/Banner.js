27c84349b716b04efb434ac60b92b7549ea4b396
abujakariacse <abujakaria316@gmail.com>
2022-06-09 01:52:23 +0600

    social icon added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import BannerImg from '../../images/banner.svg';
 import Typed from 'react-typed';
 
-
 const Banner = () => {
     return (
         <div>
@@ -23,7 +22,14 @@ const Banner = () => {
                                 loop
                             />
                         </h2>
-                        <p class="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p class="py-4 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <div className='mb-4'>
+                            <span className='text-2xl text-white mr-3 hover:text-primary transition ease-in-out duration-300'><a href="https://github.com/abujakariacse" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a></span>
+                            <span className='text-white text-2xl mr-3'><a href="https://linkedin.com/in/abujakariacse" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a></span>
+                            <span className='text-white text-2xl mr-3'><a href="https://twitter.com/abujakariacse" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i></a></span>
+                            <span className='text-white text-2xl mr-3'><a href="https://facebook.com/abujakariacse" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a></span>
+                            <span className='text-white text-2xl mr-3'><a href="https://mail.google.com/mail/u/0/?fs=1&to=abujakariacse@gmail.com&tf=cm" target="_blank" rel="noreferrer"><i class="fa-solid fa-envelope"></i></a></span>
+                        </div>
                         <button class="btn btn-primary text-white hover:bg-info">Contact Me</button>
                         <button class="btn btn-primary text-white ml-4 hover:bg-info transition ease-in-out duration-300">Get Resume</button>
                     </div>
